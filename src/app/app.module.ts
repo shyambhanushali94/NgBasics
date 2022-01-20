b1c5ce7b976d1ecee13b028f3ce42d6a2c6efa33
Jigaboy10 <jigard.bhanushali12@gmail.com>
2022-01-20 19:38:50 +0530

    created NgBasics performed property binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { Child1Component } from './components/parent/child1/child1.component';
+import { Child2Component } from './components/parent/child2/child2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    Child1Component,
+    Child2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
